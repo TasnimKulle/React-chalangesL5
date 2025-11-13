281739b392322498d85d9599cc4c0d8c5c8cf849
TasnimKulle <tasniim.tak@gmail.com>
2025-11-13 12:35:11 +0300

    adding search input

--- src/ContectApp.jsx
+++ src/ContectApp.jsx
@@ -6,17 +6,24 @@ import { ContactList } from './ContactList';
 export const ContectApp = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
   const [editContect, setEditContact] = useState(null);
+  const [searchingTerm ,setSearchingTerm]=useState('')
+  const fillterSearch=state.filter(
+    (search)=>search.name.toLowerCase().includes(searchingTerm.toLowerCase()))
 
   return (
     <div>
       <h1>Contact App</h1>
+      <input type="text" 
+      placeholder='Searching Contect..'
+      value={searchingTerm}
+      onChange={(e)=>setSearchingTerm(e.target.value)} />
       <ContectForm
         dispatch={dispatch}
         editContect={editContect}
         setEditContact={setEditContact}
       />
       <ContactList
-        contacts={state}
+        contacts={fillterSearch}
         dispatch={dispatch}
         setEditContact={setEditContact}
       />
